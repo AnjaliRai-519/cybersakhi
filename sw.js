151aca7011e1a5a7552d86a9fe66990785bc0853
AnjaliRai-519 <shivarai0001122@gmail.com>
2026-09-25 15:17:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,30 @@
-const CACHE_NAME ="cybersakhi-v1";
-const FILES_TO_CACHE = ["index.html","login.html","scam-detector.html","tips.html","report.html","emergency.html","manifest.json"];
-self.addEvenetListener("install",event =>
-  {
-    event.waitUntil(caches.open(CACHE_NAME)
-                    .then(cache => cache.addAll(FILES_TO_CACHE)));
-  });
-self.addEventListener("fetch".event => { event.respondWith(caches.match(event.reuest).then(response => response || fetch(event.request))
-                                                           );
-                                       });
+ const CACHE_NAME = "cybersakhi-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./login.html",
+    "./dashboard.html",
+    "./scam-detector.html",
+    "./tips.html",
+    "./report.html",
+    "./emergency.html",
+    "./scam-types.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
